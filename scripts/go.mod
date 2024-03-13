649633c6bcb4010d9c0a45ca6a3b40e8a33b30d2
AmitGujar <amithero3342@gmail.com>
2024-03-13 10:38:43 +0530

    path corrected

--- scripts/go.mod
+++ scripts/go.mod
@@ -1,3 +1,3 @@
-module github.com/AmitGujar/terraform-template/scripts
+module github.com/AmitGujar/terraform-boilerplate/scripts
 
 go 1.21.6
--- scripts/main.go
+++ scripts/main.go
@@ -1,12 +1,42 @@
-package main
+package terraform
 
 import (
-	"github.com/AmitGujar/terraform-template/scripts/pkgs/terraform"
+	"fmt"
+	"os"
+	"os/exec"
 )
 
-func main() {
-	terraform.Check()
-	terraform.Init()
-	terraform.Plan()
-	terraform.Apply()
+func callBack(cmd *exec.Cmd) {
+	cmd.Dir = "../"
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func Check() {
+	fmt.Println("Checking if terraform installed...")
+	cmd := exec.Command("terraform", "version")
+	callBack(cmd)
+}
+
+func Init() {
+	fmt.Println("Terraform init")
+	cmd := exec.Command("terraform", "init", "--upgrade")
+	callBack(cmd)
+}
+
+func Plan() {
+	fmt.Println("Generating terraform plan file")
+	cmd := exec.Command("terraform", "plan", "-out", "main.tfplan", "-var-file", "awssecret.tfvars")
+	callBack(cmd)
+}
+
+func Apply() {
+	fmt.Println("Applying your configuration")
+	cmd := exec.Command("terraform", "apply", "main.tfplan")
+	callBack(cmd)
 }
