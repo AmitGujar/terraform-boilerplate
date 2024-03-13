8d653e7b54bdfa5789224562135bf20aa57d680e
AmitGujar <amithero3342@gmail.com>
2024-03-13 10:40:17 +0530

    path corrected

--- scripts/main.go
+++ scripts/main.go
@@ -1,42 +1,12 @@
-package terraform
+package main
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
+	"github.com/AmitGujar/terraform-boilerplate/scripts/pkgs/terraform"
 )
 
-func callBack(cmd *exec.Cmd) {
-	cmd.Dir = "../"
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-}
-
-func Check() {
-	fmt.Println("Checking if terraform installed...")
-	cmd := exec.Command("terraform", "version")
-	callBack(cmd)
-}
-
-func Init() {
-	fmt.Println("Terraform init")
-	cmd := exec.Command("terraform", "init", "--upgrade")
-	callBack(cmd)
-}
-
-func Plan() {
-	fmt.Println("Generating terraform plan file")
-	cmd := exec.Command("terraform", "plan", "-out", "main.tfplan", "-var-file", "awssecret.tfvars")
-	callBack(cmd)
-}
-
-func Apply() {
-	fmt.Println("Applying your configuration")
-	cmd := exec.Command("terraform", "apply", "main.tfplan")
-	callBack(cmd)
+func main() {
+	terraform.Check()
+	terraform.Init()
+	terraform.Plan()
+	terraform.Apply()
 }
